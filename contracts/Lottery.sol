092d1e3962390bf0e8518774c1255e5eafcc6eb9
Robin <robin.braumann@gmail.com>
2022-02-05 05:21:40 +0100

    Combine oracle with nft minting

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,8 +1,8 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts@4.4.2/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts@4.4.2/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
 enum LotteryState {
@@ -13,21 +13,23 @@ enum LotteryState {
 }
 
 struct Entrant {
-    address entrant_address;
-    uint NFT_rarity;
+    address entrantAddress;
+    uint256 NFTRarity;
 }
 
 contract Lottery is VRFConsumerBase, Ownable, ERC1155 {
-    mapping(uint => Entrant) public entrants;
+    // moralis 
+    mapping(bytes32 => address payable) players;
     mapping(address => bool) whitelist;
     uint256 public entrantCount;
     uint256 public lotteryPot;
-    uint256 MIN_ENTRY_VALUE;
-    uint256 MAX_ENTRY_VALUE;
-    uint256 internal fee;
+    uint256 MIN_ENTRY_VALUE = 1;
+    uint256 MAX_ENTRY_VALUE = 100000;
     uint256 public categoryBracket; 
-    LotteryState public lotteryState = LotteryState.WHITELIST_ONLY;
+    LotteryState public lotteryState = LotteryState.OPEN;
     bytes32 internal keyHash;
+    uint256 internal fee;
+    uint256 internal currentUserValue;
 
     // NFT Rarities
     uint256 constant RARITY_1 = 0;
@@ -36,6 +38,9 @@ contract Lottery is VRFConsumerBase, Ownable, ERC1155 {
     uint256 constant RARITY_4 = 3;
     uint256 constant RARITY_5 = 4;
 
+    event RequestedRandomness(bytes32 id);
+    event CategoryBracket(uint256 bracket);
+    event MintTicket(address player, uint256 rarity);
 
     /**
      * Constructor inherits VRFConsumerBase
@@ -59,58 +64,64 @@ contract Lottery is VRFConsumerBase, Ownable, ERC1155 {
     /**
      * Lottery Entry
      */
+    function enter() public payable {
+        if(lotteryState == LotteryState.WHITELIST_ONLY){
+            require(whitelist[msg.sender] == true, "Sender not whitelisted");
+        }
+        require(msg.value >= MIN_ENTRY_VALUE, "Funds not sufficient");
+        mintEntrantNFT();
+    }
+
+    function mintEntrantNFT() private {
+        bytes32 id = requestRandomness(keyHash, fee);
+        players[id] = payable(msg.sender);
+        emit RequestedRandomness(id);
+    }
 
     /**
      * Callback function used by VRF Coordinator
      */
     function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
         categoryBracket = (randomness % 100) + 1;
-
-        if (categoryBracket >= 0 && categoryBracket <= 40){
+        emit CategoryBracket(categoryBracket);
+        if (categoryBracket > 0 && categoryBracket <= 40){
             // Rarity 5
-            mintTicket(RARITY_5);
+            mintTicket(RARITY_5, requestId);
         }
         else if (categoryBracket > 40 && categoryBracket <= 65){
             // Rarity 4
-            mintTicket(RARITY_4);
+            mintTicket(RARITY_4, requestId);
         }
         else if (categoryBracket > 65 && categoryBracket <= 85){
             // Rarity 3
-            mintTicket(RARITY_3);
+            mintTicket(RARITY_3, requestId);
         }
         else if (categoryBracket > 85 && categoryBracket <= 95){
             // Rarity 2
-            mintTicket(RARITY_2);
+            mintTicket(RARITY_2, requestId);
         }
         else if (categoryBracket > 95 && categoryBracket <= 100){
             // Rarity 1
-            mintTicket(RARITY_1);
+            mintTicket(RARITY_1, requestId);
         }
     }
 
-    function enter() public payable {
-        if(lotteryState == LotteryState.WHITELIST_ONLY){
-            require(whitelist[msg.sender] == true, "Sender not whitelisted");
-        }
-        require(msg.value >= MIN_ENTRY_VALUE, "Funds not sufficient");
-        require(msg.value < MAX_ENTRY_VALUE, "Funds exceed maximum entry amount for a single wallet");
-        mintEntrantNFT();
-    }
-
-    function mintEntrantNFT() public {
-        requestRandomness(keyHash, fee);
-    }
-
-    function mintTicket(uint rarity) private {
+    function mintTicket(uint256 rarity, bytes32 requestId) private {
         require(balanceOf(msg.sender, RARITY_1) == 0,"you already have a ticket");
         require(balanceOf(msg.sender, RARITY_2) == 0,"you already have a ticket");
         require(balanceOf(msg.sender, RARITY_3) == 0,"you already have a ticket");
         require(balanceOf(msg.sender, RARITY_4) == 0,"you already have a ticket");
         require(balanceOf(msg.sender, RARITY_5) == 0,"you already have a ticket");
 
-        _mint(msg.sender, rarity, 1, "0x000");
-        entrants[entrantCount] = Entrant(msg.sender, rarity);
+        _mint(players[requestId], rarity, 1, "");
+        emit MintTicket(players[requestId], rarity);
         entrantCount += 1;
+        // Pot not working Fix tomorrow
+        lotteryPot += currentUserValue;
+    }
+
+    function drawWinner(uint256 rarity) public onlyOwner payable {
+        require(balanceOf(msg.sender, rarity) == 1, "Address does not own valid ticket");
     }
 
     /**
